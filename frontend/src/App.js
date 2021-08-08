2341bfe07690791bed273f555a3aaf6a59f36367
dalakoti07 <saurabhdalakoti07@gmail.com>
2021-08-09 01:05:50 +0530

    added file pond

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
+import {useState} from 'react';
 import HomePage from "./components/Upload";
 import FooterComponent from "./components/Footer";
 import Loading from "./components/Loading";
 import ResultComponent from "./components/Result";
 
 function App() {
+  const [isUploading, setIsUploading]= useState(false);
+  const [isCompleted, setIsCompleted]= useState(false);
+  const [error,setError] = useState("");
+
   return (
     <div class="relative px-10 min-h-screen bg-gray-200 flex items-center justify-center">
-      <HomePage />
+      {/* pass setIsUploading, setError and setIsCompleted to HomePage Component, and  */}
+      {
+          isUploading? <Loading/> : (isCompleted? <isCompleted/> : <HomePage/>)
+      }
       <FooterComponent />
     </div>
   );
--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function mainUploadComponent() {
+// Import React FilePond
+import { FilePond, File, registerPlugin } from "react-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
+function MainUploadComponent() {
+  const [files, setFiles] = useState([]);
+  console.log(files)
   return (
     <div
       className="bg-white w-full md:w-1/2 
@@ -12,15 +30,24 @@ function mainUploadComponent() {
       <p1>Upload your image</p1>
       <p1>File should be Jpeg, Png, etc</p1>
 
-      <div className="w-100 bg-green-800"></div>
+      <div className="w-full">
+        <FilePond        
+          files={files}
+          onupdatefiles={setFiles}
+          allowMultiple={false}
+          maxFiles={1}
+          name="file"
+          labelIdle='Drag n Drop your files or <span class="filepond--label-action">Browse</span>'
+        />
+      </div>
 
-      <p>Or</p>
-
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">
-        Choose a File
+      <button 
+        onClick ={()=> console.log('clicked')}
+        className = {`text-white font-bold py-2 px-4 rounded-xl ${ (files.length===0) ? 'bg-gray-500 ' :'bg-blue-500 hover:bg-blue-700 motion-safe:hover:scale-110' }`} disabled={files.length===0}>
+        Upload
       </button>
     </div>
   );
 }
 
-export default mainUploadComponent;
+export default MainUploadComponent;
